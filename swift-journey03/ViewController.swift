1f028b0ee6dfd71a8e7cb75c1a333e4df5f76fa7
Leonardo Kameya <leonardo.kameya@gmail.com>
2018-08-10 15:16:39 -0400

    Adding project

--- swift-journey03/ViewController.swift
+++ swift-journey03/ViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
+    var audioPlayer : AVAudioPlayer!
+    var selectedSoundFileName : String = ""
+
+    let soundArray = ["kick1", "snare1", "hihat1", "tom1", "tom2", "ride1", "crash1"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func itemPressed(_ sender: UIButton) {
+        selectedSoundFileName = soundArray[sender.tag - 1]
+        
+        playSound()
     }
-
-
+    
+    func playSound() {
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch {
+            print(error)
+        }
+        
+        audioPlayer.play()
+    }
 }
 
